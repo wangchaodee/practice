1afcd5aa1e32803ed0a1d631e6db6acbb320e1cd
wangchaodee <wangchaodee@hotmail.com>
2023-03-30 17:55:09 +0800

    servlet

---
+++ service/demo-servlet/src/main/java/com/iflytek/staff/chao/function/SendImageServlet.java
@@ -0,0 +1,58 @@
+package com.iflytek.staff.chao.function;
+
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+/**
+ * @author : wangchaodee
+ * @Description: xxx
+ */
+@WebServlet(name ="SendImageServlet" ,urlPatterns = "/image")
+public class SendImageServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        ServletContext sc = getServletContext();
+
+        try (InputStream is = sc.getResourceAsStream("image/sid.jpg")) {
+
+            // it is the responsibility of the container to close output stream
+            OutputStream os = response.getOutputStream();
+
+            if (is == null) {
+
+                response.setContentType("text/plain");
+                os.write("Failed to send image".getBytes());
+            } else {
+
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+
+                response.setContentType("image/png");
+
+                while ((bytesRead = is.read(buffer)) != -1) {
+
+                    os.write(buffer, 0, bytesRead);
+                }
+            }
+        }
+    }
+}
